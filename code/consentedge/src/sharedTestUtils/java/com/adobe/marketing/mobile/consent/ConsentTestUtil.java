dfda9d98546fa0d38613c95d2e75e3a756b92240
pravin <pravinpk0707@gmail.com>
2021-03-10 12:48:54 -0800

    [AMSDK-11143] - Move all the test utils to shared sourceset

--- code/consentedge/src/sharedTestUtils/java/com/adobe/marketing/mobile/consent/ConsentTestUtil.java
+++ code/consentedge/src/sharedTestUtils/java/com/adobe/marketing/mobile/consent/ConsentTestUtil.java
@@ -11,10 +11,28 @@
 
 package com.adobe.marketing.mobile.consent;
 
+import com.adobe.marketing.mobile.AdobeCallbackWithError;
+import com.adobe.marketing.mobile.AdobeError;
+import com.adobe.marketing.mobile.Event;
+import com.adobe.marketing.mobile.LoggingMode;
+import com.adobe.marketing.mobile.MobileCore;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 class ConsentTestUtil {
 
@@ -182,6 +200,115 @@ class ConsentTestUtil {
             return null;
         }
         return contentMap.get("val");
+    }
+
+    static Map<String, Object> getConsentsSync(){
+        try {
+            final HashMap<String,Object> getConsentResponse = new HashMap<String, Object>();
+            final CountDownLatch latch = new CountDownLatch(1);
+            Consent.getConsents(new AdobeCallbackWithError<Map<String, Object>>() {
+                @Override
+                public void call(Map<String, Object> consents) {
+                    getConsentResponse.put(ConsentTestConstants.GetConsentHelper.VALUE, consents);
+                    latch.countDown();
+                }
+
+                @Override
+                public void fail(AdobeError adobeError) {
+                    getConsentResponse.put(ConsentTestConstants.GetConsentHelper.ERROR, adobeError);
+                    latch.countDown();
+                }
+            });
+            latch.await();
+
+            return  getConsentResponse;
+        } catch (Exception exp) {
+            return null;
+        }
+    }
+
+    static void applyDefaultConsent(final Map defaultConsentMap) {
+        HashMap<String, Object> config = new HashMap<String, Object>() {
+            {
+                put(ConsentConstants.ConfigurationKey.DEFAULT_CONSENT, defaultConsentMap);
+            }
+        };
+        MobileCore.updateConfiguration(config);
+    }
+
+    static Event buildEdgeConsentPreferenceEventWithConsents(final Map<String,Object> consents) {
+        List<Map<String,Object>> payload = new ArrayList<>();
+        payload.add((Map)(consents.get("consents")));
+        Map<String,Object> eventData = new HashMap<>();
+        eventData.put("payload", payload);
+        eventData.put("type", "consent:preferences");
+        return new Event.Builder("Edge Consent Preference", ConsentConstants.EventType.EDGE, ConsentConstants.EventSource.CONSENT_PREFERENCE).setEventData(eventData).build();
+    }
+
+    static Event buildEdgeConsentPreferenceEvent(final String jsonString) throws JSONException {
+        Map<String, Object> eventData = Utility.toMap(new JSONObject(jsonString));
+        return new Event.Builder("Edge Consent Preference", ConsentConstants.EventType.EDGE, ConsentConstants.EventSource.CONSENT_PREFERENCE).setEventData(eventData).build();
+    }
+
+    /**
+     * Serialize the given {@code map} to a JSON Object, then flattens to {@code Map<String, String>}.
+     * For example, a JSON such as "{xdm: {stitchId: myID, eventType: myType}}" is flattened
+     * to two map elements "xdm.stitchId" = "myID" and "xdm.eventType" = "myType".
+     * @param map map with JSON structure to flatten
+     * @return new map with flattened structure
+     */
+    static Map<String, String> flattenMap(final Map<String, Object> map) {
+        if (map == null || map.isEmpty()) {
+            return Collections.<String, String>emptyMap();
+        }
+
+        try {
+            JSONObject jsonObject = new JSONObject(map);
+            Map<String, String> payloadMap = new HashMap<>();
+            addKeys("", new ObjectMapper().readTree(jsonObject.toString()), payloadMap);
+            return payloadMap;
+        } catch (IOException e) {
+            MobileCore.log(LoggingMode.ERROR, "FunctionalTestUtils", "Failed to parse JSON object to tree structure.");
+        }
+
+        return Collections.<String, String>emptyMap();
+    }
+
+
+    /**
+     * Deserialize {@code JsonNode} and flatten to provided {@code map}.
+     * For example, a JSON such as "{xdm: {stitchId: myID, eventType: myType}}" is flattened
+     * to two map elements "xdm.stitchId" = "myID" and "xdm.eventType" = "myType".
+     *
+     * Method is called recursively. To use, call with an empty path such as
+     * {@code addKeys("", new ObjectMapper().readTree(JsonNodeAsString), map);}
+     *
+     * @param currentPath the path in {@code JsonNode} to process
+     * @param jsonNode {@link JsonNode} to deserialize
+     * @param map {@code Map<String, String>} instance to store flattened JSON result
+     *
+     * @see <a href="https://stackoverflow.com/a/24150263">Stack Overflow post</a>
+     */
+    private static void addKeys(String currentPath, JsonNode jsonNode, Map<String, String> map) {
+        if (jsonNode.isObject()) {
+            ObjectNode objectNode = (ObjectNode) jsonNode;
+            Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
+            String pathPrefix = currentPath.isEmpty() ? "" : currentPath + ".";
+
+            while (iter.hasNext()) {
+                Map.Entry<String, JsonNode> entry = iter.next();
+                addKeys(pathPrefix + entry.getKey(), entry.getValue(), map);
+            }
+        } else if (jsonNode.isArray()) {
+            ArrayNode arrayNode = (ArrayNode) jsonNode;
+
+            for (int i = 0; i < arrayNode.size(); i++) {
+                addKeys(currentPath + "[" + i + "]", arrayNode.get(i), map);
+            }
+        } else if (jsonNode.isValueNode()) {
+            ValueNode valueNode = (ValueNode) jsonNode;
+            map.put(currentPath, valueNode.asText());
+        }
     }
 
     private static Map<String, Object> getAllConsentsMap(Consents consents) {
